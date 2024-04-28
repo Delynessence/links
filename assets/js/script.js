0f9a3685f0ee83ba3bd4ddcc7a1e6784bf3e6e34
Delynessence <135038394+Delynessence@users.noreply.github.com>
2024-04-28 23:11:50 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,4 +46,37 @@ function changeImage() {
     }
     
     
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+    /*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('#Photo, #Photo-2, #Name, #Description, iframe',{}); 
+sr.reveal('#title, #linksbutton, #footertext',{delay: 400}); 
+
 })();
